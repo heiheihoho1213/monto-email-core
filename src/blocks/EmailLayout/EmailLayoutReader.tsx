0e8d8d6444489b8df9811a18291b553b91ce06cd
heng.du <heng.du@ucloud.cn>
2026-04-22 19:40:10 +0800

    fix: 字体样式优化

--- src/blocks/EmailLayout/EmailLayoutReader.tsx
+++ src/blocks/EmailLayout/EmailLayoutReader.tsx
@@ -8,23 +8,23 @@ function getFontFamily(fontFamily: EmailLayoutProps['fontFamily']) {
   const f = fontFamily ?? 'MODERN_SANS';
   switch (f) {
     case 'MODERN_SANS':
-      return '"Helvetica Neue", "Arial Nova", "Nimbus Sans", Arial, sans-serif';
+      return 'Helvetica, Arial, sans-serif';
     case 'BOOK_SANS':
-      return 'Optima, Candara, "Noto Sans", source-sans-pro, sans-serif';
+      return 'Optima, Candara, source-sans-pro, sans-serif';
     case 'ORGANIC_SANS':
-      return 'Seravek, "Gill Sans Nova", Ubuntu, Calibri, "DejaVu Sans", source-sans-pro, sans-serif';
+      return 'Seravek, Ubuntu, Calibri, source-sans-pro, sans-serif';
     case 'GEOMETRIC_SANS':
-      return 'Avenir, "Avenir Next LT Pro", Montserrat, Corbel, "URW Gothic", source-sans-pro, sans-serif';
+      return 'Avenir, Montserrat, Corbel, source-sans-pro, sans-serif';
     case 'HEAVY_SANS':
-      return 'Bahnschrift, "DIN Alternate", "Franklin Gothic Medium", "Nimbus Sans Narrow", sans-serif-condensed, sans-serif';
+      return 'Bahnschrift, sans-serif-condensed, sans-serif';
     case 'ROUNDED_SANS':
-      return 'ui-rounded, "Hiragino Maru Gothic ProN", Quicksand, Comfortaa, Manjari, "Arial Rounded MT Bold", Calibri, source-sans-pro, sans-serif';
+      return 'ui-rounded, Quicksand, Comfortaa, Manjari, Calibri, source-sans-pro, sans-serif';
     case 'MODERN_SERIF':
-      return 'Charter, "Bitstream Charter", "Sitka Text", Cambria, serif';
+      return 'Charter, Cambria, serif';
     case 'BOOK_SERIF':
-      return '"Iowan Old Style", "Palatino Linotype", "URW Palladio L", P052, serif';
+      return 'P052, serif';
     case 'MONOSPACE':
-      return '"Nimbus Mono PS", "Courier New", "Cutive Mono", monospace';
+      return 'monospace';
   }
 }
 
